54ba740eb63e2fe6cbdafd06801691290bc2fc06
Mike Strand <mxstrand@hotmail.com>
2014-08-09 20:36:26 -0700

    clean up

--- accelerometerWithTFTDisplay.ino
+++ accelerometerWithTFTDisplay.ino
@@ -177,13 +177,13 @@ void loop() {
   Serial.println(mode);
   switch (mode) {
     case 0:
-      printAccereometerValues();
+      printAccelerometerValues();
       break;
     case 1:
       printTemperatureValues();
       break;
     case 2:
-       printMicVoltageValues();
+      printMicVoltageValues();
       break;      
   }    
 
@@ -196,7 +196,7 @@ void clearTheScreen() {
   tft.setTextSize(0); 
 }
 
-void printAccereometerValues() {
+void printAccelerometerValues() {
   clearTheScreen();
   int maxScreenLineCount = 16;
   
@@ -207,7 +207,7 @@ void printAccereometerValues() {
     accel.getEvent(&event);
     
     /* Display the results (acceleration is measured in m/s^2) */
-    printMode();
+    //printMode();
     tft.setTextColor(ST7735_BLUE);
     tft.print("X:"); tft.print(event.acceleration.x); tft.print(" ");
     tft.setTextColor(ST7735_GREEN);
@@ -239,7 +239,7 @@ void printTemperatureValues() {
                                                    //to degrees ((voltage - 500mV) times 100)
      //tft.print(temperatureC); tft.println(" degrees C");
      
-     printMode();
+     //printMode();
      // now convert to Fahrenheit
      float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
      tft.print(temperatureF); tft.println(" degrees F");
@@ -278,193 +278,12 @@ void printMicVoltageValues() {
      peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
      double volts = (peakToPeak * 3.3) / 1024;  // convert to volts
      
-     printMode();
+     //printMode();
      tft.print("Volume volate is: ");
      tft.println(volts);
      getVolumeVoltage();
   }  
    
-}
-
-void testlines(uint16_t color) {
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(0, 0, x, tft.height()-1, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(0, 0, tft.width()-1, y, color);
-  }
-
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(tft.width()-1, 0, x, tft.height()-1, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(tft.width()-1, 0, 0, y, color);
-  }
-
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(0, tft.height()-1, x, 0, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(0, tft.height()-1, tft.width()-1, y, color);
-  }
-
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(tft.width()-1, tft.height()-1, x, 0, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(tft.width()-1, tft.height()-1, 0, y, color);
-  }
-}
-
-void testdrawtext(char *text, uint16_t color) {
-  tft.setCursor(0, 0);
-  tft.setTextColor(color);
-  tft.setTextWrap(true);
-  tft.print(text);
-}
-
-void testfastlines(uint16_t color1, uint16_t color2) {
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t y=0; y < tft.height(); y+=5) {
-    tft.drawFastHLine(0, y, tft.width(), color1);
-  }
-  for (int16_t x=0; x < tft.width(); x+=5) {
-    tft.drawFastVLine(x, 0, tft.height(), color2);
-  }
-}
-
-void testdrawrects(uint16_t color) {
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color);
-  }
-}
-
-void testfillrects(uint16_t color1, uint16_t color2) {
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=tft.width()-1; x > 6; x-=6) {
-    tft.fillRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color1);
-    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color2);
-  }
-}
-
-void testfillcircles(uint8_t radius, uint16_t color) {
-  for (int16_t x=radius; x < tft.width(); x+=radius*2) {
-    for (int16_t y=radius; y < tft.height(); y+=radius*2) {
-      tft.fillCircle(x, y, radius, color);
-    }
-  }
-}
-
-void testdrawcircles(uint8_t radius, uint16_t color) {
-  for (int16_t x=0; x < tft.width()+radius; x+=radius*2) {
-    for (int16_t y=0; y < tft.height()+radius; y+=radius*2) {
-      tft.drawCircle(x, y, radius, color);
-    }
-  }
-}
-
-void testtriangles() {
-  tft.fillScreen(ST7735_BLACK);
-  int color = 0xF800;
-  int t;
-  int w = 63;
-  int x = 159;
-  int y = 0;
-  int z = 127;
-  for(t = 0 ; t <= 15; t+=1) {
-    tft.drawTriangle(w, y, y, x, z, x, color);
-    x-=4;
-    y+=4;
-    z-=4;
-    color+=100;
-  }
-}
-
-void testroundrects() {
-  tft.fillScreen(ST7735_BLACK);
-  int color = 100;
-  int i;
-  int t;
-  for(t = 0 ; t <= 4; t+=1) {
-    int x = 0;
-    int y = 0;
-    int w = 127;
-    int h = 159;
-    for(i = 0 ; i <= 24; i+=1) {
-      tft.drawRoundRect(x, y, w, h, 5, color);
-      x+=2;
-      y+=3;
-      w-=4;
-      h-=6;
-      color+=1100;
-    }
-    color+=100;
-  }
-}
-
-void tftPrintTest() {
-  tft.setTextWrap(false);
-  tft.fillScreen(ST7735_BLACK);
-  tft.setCursor(0, 30);
-  tft.setTextColor(ST7735_RED);
-  tft.setTextSize(1);
-  tft.println("Hello World!");
-  tft.setTextColor(ST7735_YELLOW);
-  tft.setTextSize(2);
-  tft.println("Hello World!");
-  tft.setTextColor(ST7735_GREEN);
-  tft.setTextSize(3);
-  tft.println("Hello World!");
-  tft.setTextColor(ST7735_BLUE);
-  tft.setTextSize(4);
-  tft.print(1234.567);
-  delay(1500);
-  tft.setCursor(0, 0);
-  tft.fillScreen(ST7735_BLACK);
-  tft.setTextColor(ST7735_WHITE);
-  tft.setTextSize(0);
-  tft.println("Hello World!");
-  tft.setTextSize(1);
-  tft.setTextColor(ST7735_GREEN);
-  tft.print(p, 6);
-  tft.println(" Want pi?");
-  tft.println(" ");
-  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
-  tft.println(" Print HEX!");
-  tft.println(" ");
-  tft.setTextColor(ST7735_WHITE);
-  tft.println("Sketch has been");
-  tft.println("running for: ");
-  tft.setTextColor(ST7735_MAGENTA);
-  tft.print(millis() / 1000);
-  tft.setTextColor(ST7735_WHITE);
-  tft.print(" seconds.");
-}
-
-void mediabuttons() {
-  // play
-  tft.fillScreen(ST7735_BLACK);
-  tft.fillRoundRect(25, 10, 78, 60, 8, ST7735_WHITE);
-  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_RED);
-  delay(500);
-  // pause
-  tft.fillRoundRect(25, 90, 78, 60, 8, ST7735_WHITE);
-  tft.fillRoundRect(39, 98, 20, 45, 5, ST7735_GREEN);
-  tft.fillRoundRect(69, 98, 20, 45, 5, ST7735_GREEN);
-  delay(500);
-  // play color
-  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_BLUE);
-  delay(50);
-  // pause color
-  tft.fillRoundRect(39, 98, 20, 45, 5, ST7735_RED);
-  tft.fillRoundRect(69, 98, 20, 45, 5, ST7735_RED);
-  // play color
-  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_GREEN);
 }
 
 void displaySensorDetails(void)
@@ -593,9 +412,11 @@ void rotateText() {
 }
 
 void getVolumeVoltage() {
+  Serial.println("Gonna check the volume voltage..");  
+  
   if ( running == false ) {
     running = true;
-    Serial.println("Gonna check the volume voltage..");  
+    
     Serial.print("Mode is: ");  
     Serial.println(mode);
   
@@ -606,10 +427,10 @@ void getVolumeVoltage() {
      unsigned int signalMinV = 1024;
     
      // collect data for 50 mS
-     while (millis() - startMillisV < sampleWindowV){   
+     while (millis() - startMillisV < sampleWindowV) {   
         sampleV = analogRead(1);
-        if (sampleV < 1024)  // toss out spurious readings
-        {
+        // toss out spurious readings
+        if (sampleV < 1024) {
            if (sampleV > signalMaxV)
            {
               signalMaxV = sampleV;  // save just the max levels
@@ -622,10 +443,10 @@ void getVolumeVoltage() {
      }
      peakToPeakV = signalMaxV - signalMinV;  // max - min = peak-peak amplitude
      double voltsV = (peakToPeakV * 3.3) / 1024;  // convert to volts
-     Serial.print("I know my volts!: "); 
+     Serial.print("I know my volts: "); 
      Serial.println(voltsV);
      
-     double switchVolts = 0.60;  // volume level required to switch sensors
+     double switchVolts = 0.50;  // volume level required to switch sensors
      if ( running = true ) {
        if ( voltsV > switchVolts ) {
          Serial.println("voltsV > switchVolts: "); 
@@ -635,9 +456,13 @@ void getVolumeVoltage() {
        if ( mode == 3 ) {
          mode = 0;
        }
+       running = false;
      }
   }
-  running = false;  
+  else {
+     Serial.println("Already running. Sit tight.");
+  }  
+
 }
 
 void printMode() {
